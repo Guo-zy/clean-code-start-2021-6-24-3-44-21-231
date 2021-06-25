fd3e2d38b559c1c2c929b90109416d2f40067590
Guo-zy <674402145@qq.com>
2021-06-25 21:46:47 +0800

    rename variables

--- src/main/java/com/tw/academy/basic/$7_long_method/OrderReceipt.java
+++ src/main/java/com/tw/academy/basic/$7_long_method/OrderReceipt.java
@@ -8,46 +8,44 @@ package com.tw.academy.basic.$7_long_method;
  * @since   2018-1-1
  */
 public class OrderReceipt {
-    private Order o;
+    private Order order;
     final double SALES_TAX_RATE = 0.1d;
     final String ORDER_HEAD = "======Printing Orders======\n";
     final String SALSES_TAX = "Sales Tax";
     final String TOTAL_AMOUNT = "Total Amount";
 
-    public OrderReceipt(Order o) {
-        this.o = o;
+    public OrderReceipt(Order order) {
+        this.order = order;
     }
 
-    //todo: rename -- Tom
     public String printReceipt() {
-        StringBuilder output = new StringBuilder();
+        StringBuilder receiptDetails = new StringBuilder();
 
-        output.append(ORDER_HEAD);
+        receiptDetails.append(ORDER_HEAD);
 
-        output.append(o.getCustomerName());
-        output.append(o.getCustomerAddress());
+        receiptDetails.append(order.getCustomerName());
+        receiptDetails.append(order.getCustomerAddress());
 
-        double totSalesTx = 0d;
-        double tot = 0d;
-        for (LineItem lineItem : o.getLineItems()) {
-            output.append(lineItem.getDescription());
-            output.append('\t');
-            output.append(lineItem.getPrice());
-            output.append('\t');
-            output.append(lineItem.getQuantity());
-            output.append('\t');
-            output.append(lineItem.totalAmount());
-            output.append('\n');
+        double totalSalesTax = 0d;
+        double totalAmount = 0d;
+        for (LineItem lineItem : order.getLineItems()) {
+            receiptDetails.append(lineItem.getDescription());
+            receiptDetails.append('\t');
+            receiptDetails.append(lineItem.getPrice());
+            receiptDetails.append('\t');
+            receiptDetails.append(lineItem.getQuantity());
+            receiptDetails.append('\t');
+            receiptDetails.append(lineItem.totalAmount());
+            receiptDetails.append('\n');
 
             double salesTax = lineItem.totalAmount() * SALES_TAX_RATE;
-            totSalesTx += salesTax;
-
-            tot += lineItem.totalAmount() + salesTax;
+            totalSalesTax += salesTax;
+            totalAmount += lineItem.totalAmount() + salesTax;
         }
 
-        output.append(SALSES_TAX).append('\t').append(totSalesTx);
+        receiptDetails.append(SALSES_TAX).append('\t').append(totalSalesTax);
 
-        output.append(TOTAL_AMOUNT).append('\t').append(tot);
-        return output.toString();
+        receiptDetails.append(TOTAL_AMOUNT).append('\t').append(totalAmount);
+        return receiptDetails.toString();
     }
 }
--- src/main/java/com/tw/academy/basic/$7_long_method/readme.md
+++ src/main/java/com/tw/academy/basic/$7_long_method/readme.md
@@ -1,3 +1,4 @@
 refactor step:
     1. clean dead code
     2. extract constants(clean magic_number)
+    3. rename variables and delete todo annotation
